d6e8b1dd5516197b5bd627d0f0cf6c7b89b96202
kaenguru123 <daskaenguru1234@gmail.com>
2023-12-08 13:43:48 +0100

    singel thred

---
+++ five_singel.py
@@ -0,0 +1,39 @@
+class five():
+    def __init__(self):
+        with open('./input5.txt', 'r') as f:
+            content = f.read().split('\n\n')
+            self.seeds = content[0].split(' ')[1:]
+            self.seeds = [int(seed) for seed in self.seeds]
+            self.seeds = [self.seeds[i:i+2] for i in range(0, len(self.seeds), 2)]
+            self.maps = [map.split('\n')[1:] for map in content[1:]]
+            new_list1 = []
+            for map in self.maps:
+                new_list2 = []
+                for line in map:
+                    app = line.split(' ')
+                    if len(app) == 3:
+                        app = [int(n) for n in app]
+                    new_list2.append(app)
+                new_list1.append(new_list2)
+            self.maps = new_list1
+
+            self.locations = []
+            for seed_start, seed_range in self.seeds:
+                res=[]
+                print(f'new range: {seed_range} (start from: {seed_start})')
+                range_of_seeds = list(range(seed_start, seed_start + seed_range))
+                for seed in range_of_seeds:
+                    for map in self.maps:
+                        seed = self.mapping(seed, map)
+                    res.append(seed)
+                self.locations.append(min(res))
+            print(f"lowest location_id: {min(self.locations)}")
+
+    def mapping(self, seed, map):
+        for dest, src, range in map:
+            if seed >= src and seed < src+range:
+                return dest-src+seed
+        return seed
+
+if __name__ == "__main__":
+        main = five()
